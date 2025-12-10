af48d222ae21d2ba86c95ed5b01d390272a01166
Shankalpa Duwadi <duwadishankalpa@gmail.com>
2025-12-10 13:36:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-import React, { JSX } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { JSX, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 export default function App() {
-	interface WeatherProps {
-		weather: string;
-	}
-	
-	type WeatherState = {
-		count: number;
-	};
-	class WeatherComponent extends React.Component<WeatherProps, WeatherState> {
-		constructor(props: WeatherProps) {
-			super(props);
-			this.state = {
-			count: 0
-			};
-		}
-		componentDidMount() {
-			this.setState({ count: 1 });
-		}
-		clickHandler(): void {
-			this.setState({ count: this.state.count + 1 });
-		}
-		render() {
-			return (
-				<h1 onClick={() => this.clickHandler()}>
-					The weather is {this.props.weather}, and the counter shows{" "}
-					{this.state.count}
-				</h1>
-			);
-		}
-	}
-	return (<WeatherComponent weather="sunny" />);
+  interface WeatherProps {
+    weather: string;
+  }
+  const WeatherComponent = (props: WeatherProps): JSX.Element => {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+      setCount(1);
+    }, []);
+    return (
+      <h1 onClick={() => setCount(count + 1)}>
+        The weather is {props.weather}, and the counter shows {count}
+      </h1>
+    );
+  };
+  return <WeatherComponent weather="sunny" />;
 }
